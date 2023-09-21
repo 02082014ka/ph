75f28ef2d8977882249d24b47f3b3284110bf39c
Alex Gagarin nunes <142193528+02082014ka@users.noreply.github.com>
2023-09-20 23:25:45 -0300

    Add files via upload

---
+++ teste-remoto-MT-TI-Alex nunes/rodape.php
@@ -0,0 +1,13 @@
+<!--Alex Gagarin Nunes  candidato vaga de Desenvolvedor de Sistemas.-->
+
+<div class="footer">
+	<div class="footer-inner">
+	2023 &copy; Multidados TI.
+	</div>
+	<div class="footer-tools">
+	<span class="go-top">
+	<i class="fa fa-angle-up">
+	</i>
+    </span>
+   </div>
+</div>
